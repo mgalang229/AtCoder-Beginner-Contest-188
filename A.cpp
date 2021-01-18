229125380e146bec863861d5eb020d389a682f21
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-18 18:38:35 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -7,6 +7,7 @@ int main() {
 	cin.tie(0);
 	int x, y;
 	cin >> x >> y;
+	// check if the difference of x and y is less than 3, this means that min(x, y) can overtake max(x, y)
 	cout << (abs(x - y) < 3 ? "Yes" : "No") << '\n';
 	return 0; 
 }
